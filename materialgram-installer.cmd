7c50a12c9c766e1d59ab5e5f01a60081a02c2635
Lucas Gabriel <90426410+lucmsilva651@users.noreply.github.com>
2024-04-25 22:53:22 -0300

    Update materialgram-installer.cmd

--- materialgram-installer.cmd
+++ materialgram-installer.cmd
@@ -1,9 +1,9 @@
 @echo off
 chcp 65001
 
-rmdir /q /s %LOCALAPPDATA%\Materialgram
-mkdir %LOCALAPPDATA%\Materialgram
-cd %LOCALAPPDATA%\Materialgram
+rmdir /q /s %LOCALAPPDATA%\materialgram
+mkdir %LOCALAPPDATA%\materialgram
+cd %LOCALAPPDATA%\materialgram
 
 for /f "tokens=*" %%i in ('curl -s https://api.github.com/repos/kukuruzka165/materialgram/releases/latest ^| findstr "browser_download_url.*win64_materialgram"') do (
     set downloadUrl=%%i
@@ -13,19 +13,19 @@ set downloadUrl=%downloadUrl:"=%
 set downloadUrl=%downloadUrl:browser_download_url:=%
 set downloadUrl=%downloadUrl: ,=%
 
-curl -L %downloadUrl% -o win64_materialgram_latest.zip
+curl -L %downloadUrl% -o latest.zip
 
-set "zipFile=win64_materialgram_latest.zip"
+set "zipFile=latest.zip"
 powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%zipFile%', '%~dp0'); }"
 
 cd %~dp0
-move /y Materialgram.exe %LOCALAPPDATA%\Materialgram\Materialgram.exe
-move /y Updater.exe %LOCALAPPDATA%\Materialgram\Updater.exe 
+move /y Materialgram.exe %LOCALAPPDATA%\materialgram\materialgram.exe
+move /y Updater.exe %LOCALAPPDATA%\materialgram\updater.exe 
 
-nircmd-x64\nircmd.exe shortcut "%LOCALAPPDATA%\Materialgram\Materialgram.exe" "~$folder.programs$" "Materialgram"
-nircmd-x64\nircmd.exe shortcut "%LOCALAPPDATA%\Materialgram\Materialgram.exe" "~$folder.desktop$" "Materialgram"
+nircmd-x64\nircmd.exe shortcut "%LOCALAPPDATA%\materialgram\materialgram.exe" "~$folder.programs$" "materialgram"
+nircmd-x64\nircmd.exe shortcut "%LOCALAPPDATA%\materialgram\materialgram.exe" "~$folder.desktop$" "materialgram"
 
-del /f "%LOCALAPPDATA%\Materialgram\win64_materialgram_latest.zip"
+del /f "%LOCALAPPDATA%\materialgram\latest.zip"
 
-start %LOCALAPPDATA%\Materialgram\Materialgram.exe
+start %LOCALAPPDATA%\materialgram\materialgram.exe
 exit
