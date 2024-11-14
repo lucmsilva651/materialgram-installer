6d8616af2c0db9374dff82dc793c9f2baf79c136
Lucas Gabriel <lucmsilva651@gmail.com>
2024-11-14 18:58:13 -0300

    Updated installer + removed NirCmd from deps (download it from web)

--- materialgram-installer.cmd
+++ materialgram-installer.cmd
@@ -1,10 +1,12 @@
 @echo off
-chcp 65001
+chcp 65001 > nul
 
+echo Removing old installs of materialgram...
 rmdir /q /s %LOCALAPPDATA%\materialgram
-mkdir %LOCALAPPDATA%\materialgram
-cd %LOCALAPPDATA%\materialgram
+rmdir /q /s %LOCALAPPDATA%\NirCmd
+cls
 
+echo Gathering latest release from GitHub API...
 for /f "tokens=*" %%i in ('curl -s https://api.github.com/repos/kukuruzka165/materialgram/releases/latest ^| findstr "browser_download_url.*win64_materialgram"') do (
     set downloadUrl=%%i
 )
@@ -13,19 +15,25 @@ set downloadUrl=%downloadUrl:"=%
 set downloadUrl=%downloadUrl:browser_download_url:=%
 set downloadUrl=%downloadUrl: ,=%
 
-curl -L %downloadUrl% -o latest.zip
+echo Downloading from GitHub...
+curl -L %downloadUrl% -o %LOCALAPPDATA%\materialgram.zip
 
-set "zipFile=latest.zip"
-powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%zipFile%', '%~dp0'); }"
+echo Extracting from ZIP...
+powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%LOCALAPPDATA%\materialgram.zip', '%LOCALAPPDATA%\materialgram'); }"
 
-cd %~dp0
-move /y Materialgram.exe %LOCALAPPDATA%\materialgram\materialgram.exe
-move /y Updater.exe %LOCALAPPDATA%\materialgram\updater.exe 
+echo Downloading NirCmd to do shortcuts...
+powershell -nologo -noprofile -command "curl https://www.nirsoft.net/utils/nircmd-x64.zip -o %LOCALAPPDATA%\NirCmd.zip"
+powershell -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%LOCALAPPDATA%\NirCmd.zip', '%LOCALAPPDATA%\NirCmd'); }"
 
-nircmd-x64\nircmd.exe shortcut "%LOCALAPPDATA%\materialgram\materialgram.exe" "~$folder.programs$" "materialgram"
-nircmd-x64\nircmd.exe shortcut "%LOCALAPPDATA%\materialgram\materialgram.exe" "~$folder.desktop$" "materialgram"
+echo Doing shortcuts...
+%LOCALAPPDATA%\NirCmd\nircmd.exe shortcut "%LOCALAPPDATA%\materialgram\materialgram.exe" "~$folder.programs$" "materialgram"
+%LOCALAPPDATA%\NirCmd\nircmd.exe shortcut "%LOCALAPPDATA%\materialgram\materialgram.exe" "~$folder.desktop$" "materialgram"
 
-del /f "%LOCALAPPDATA%\materialgram\latest.zip"
+echo Removing temp file...
+del /f "%LOCALAPPDATA%\NirCmd.zip"
+del /f "%LOCALAPPDATA%\materialgram.zip"
+rmdir /q /s %LOCALAPPDATA%\NirCmd
 
+echo Starting materialgram...
 start %LOCALAPPDATA%\materialgram\materialgram.exe
 exit
